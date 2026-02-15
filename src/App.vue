8d381dade3c18d76a5357fbac05f962021fec479
shunezulad <knitevision1@gmail.com>
2026-02-15 14:28:16 +0100

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const foo = ref([1, 2, 3])
+
+function update() {
+  foo.value.push(9)
+}
+
+onMounted(() => {
+  update()
+})
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+    {{ foo }}
+  </p>
+</template>
+
+<style scoped></style>
